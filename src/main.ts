5280cde65546fe029bbf47890b4126c22fde865a
akira2542 <akiraakamura@gmail.com>
2022-05-27 16:39:27 +0700

    response transformer

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseTransform } from './utils/interceptors/response-transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'debug'],
+  });
+  app.useGlobalInterceptors(new ResponseTransform());
+  app.enableCors({
+    allowedHeaders:"*",
+    origin: "*"
   });
   app.useGlobalPipes(
     new ValidationPipe({
